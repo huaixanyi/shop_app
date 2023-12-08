0ef0fc4d10537a981b8912b0b21aac837f26c777
画仙衣 <40336670+huaixanyi@users.noreply.github.com>
2023-12-08 15:48:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 // 监听端口
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://jazzy-cassata-bdf5f8.netlify.app:${PORT}/`);
 });
